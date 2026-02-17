30adf2729f7004e0d88b94aeb662934823c9cab2
Dusanapudi <2464207@cognizant.com>
2026-02-18 02:34:12 +0530

    Add Mochawesome reporter for Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  }
+})
